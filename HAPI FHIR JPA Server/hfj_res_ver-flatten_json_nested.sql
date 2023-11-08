233d6d134c3f82ecf6ad25abbacc4a170afa9d4c
Levi <muhammad.levi@sid-indonesia.org>
2023-11-08 11:32:27 +0700

    Affected Issue(s): Fix max 1600 columns
    What this commit has achieved:
    1. Flattened each resource type

--- HAPI FHIR JPA Server/hfj_res_ver-flatten_json_nested.sql
+++ HAPI FHIR JPA Server/hfj_res_ver-flatten_json_nested.sql
@@ -8,30 +8,57 @@ CREATE OR REPLACE FUNCTION generated_views.create_json_flat_view (
     identifier_value TEXT,
     identifier_column TEXT
   ) RETURNS TEXT LANGUAGE plpgsql AS $$DECLARE cols TEXT;
-BEGIN execute format (
+BEGIN EXECUTE format(
+  $ex$ DROP VIEW IF EXISTS generated_views."z_flattened_%4$s_all_versions_view";
+CREATE VIEW generated_views."z_flattened_%4$s_all_versions_view" AS with recursive flat (pid, key, value) as (
+  select pid,
+    key,
+    value
+  from %1$s hrv,
+    jsonb_each("%2$S"::jsonb)
+  where hrv.%4$s = %3$L
+  union
+  select f.pid,
+    concat(f.key, '.', j.key),
+    j.value
+  from flat f,
+    jsonb_each(f.value) j
+  where jsonb_typeof(f.value) = 'object'
+)
+select pid,
+  jsonb_object_agg(key, value) as "%4$s"
+from flat
+where jsonb_typeof(value) <> 'object'
+group by pid;
+$ex$,
+table_name,
+json_column,
+identifier_value,
+identifier_column
+);
+execute format (
   $ex$
   select string_agg(
-      format('aravv.data->>%%1$L "%1$s.%%1$s"', key),
+      format('aravv."%1$s"->>%%1$L "%1$s.%%1$s"', key),
       ', '
     )
   from (
       select distinct key
-      from generated_views."All_resources_all_versions_view" aravv,
-        jsonb_each(data)
-      WHERE aravv.data->>'resourceType' = %1$L
+      from generated_views."z_flattened_%1$s_all_versions_view" aravv,
+        jsonb_each("$1$s") -- WHERE aravv."%4$s"->>'resourceType' = %1$L
       order by 1
     ) s;
 $ex$,
 identifier_value
 ) into cols;
-EXECUTE format(
+execute format (
   $ex$ DROP VIEW IF EXISTS generated_views."%4$s_all_versions_view";
 CREATE VIEW generated_views."%4$s_all_versions_view" AS
 SELECT %2$s,
   %6$s,
   CONCAT(%4$L, '/', hrv.res_id) AS "%4$s.referenceString"
 FROM %1$s hrv
-  RIGHT JOIN generated_views."All_resources_all_versions_view" aravv ON hrv.pid = aravv.pid
+  RIGHT JOIN generated_views."z_flattened_%4$s_all_versions_view" aravv ON hrv.pid = aravv.pid
 WHERE hrv.%5$s = %4$L $ex$,
   table_name,
   regular_columns,
@@ -47,41 +74,40 @@ END $$;
 CREATE OR REPLACE FUNCTION generated_views.generate_all_fhir_resources_views() RETURNS TEXT LANGUAGE plpgsql AS $$
 DECLARE the_queries TEXT;
 BEGIN EXECUTE format (
-  $ex$
-  CREATE OR REPLACE VIEW generated_views."All_resources_all_versions_view" AS --
--- https://stackoverflow.com/a/45592457
-    with recursive flat (pid, key, value) as (
-      select pid,
-        key,
-        value
-      from public.hfj_res_ver hrv,
-        jsonb_each(res_text_vc::jsonb)
-      union
-      select f.pid,
-        concat(f.key, '.', j.key),
-        j.value
-      from flat f,
-        jsonb_each(f.value) j
-      where jsonb_typeof(f.value) = 'object'
+  $ex$ -- CREATE OR REPLACE VIEW generated_views."All_resources_all_versions_view" AS --
+  -- -- https://stackoverflow.com/a/45592457
+  --     with recursive flat (pid, key, value) as (
+  --       select pid,
+  --         key,
+  --         value
+  --       from public.hfj_res_ver hrv,
+  --         jsonb_each(res_text_vc::jsonb)
+  --       union
+  --       select f.pid,
+  --         concat(f.key, '.', j.key),
+  --         j.value
+  --       from flat f,
+  --         jsonb_each(f.value) j
+  --       where jsonb_typeof(f.value) = 'object'
+  --     )
+  --   select pid,
+  --     jsonb_object_agg(key, value) as data
+  --   from flat
+  --   where jsonb_typeof(value) <> 'object'
+  --   group by pid;
+  SELECT string_agg(
+      format(
+        'generated_views.create_json_flat_view(''public.hfj_res_ver'',''hrv.pid, partition_date, partition_id, res_deleted_at, res_version, has_tags, res_published, res_updated, res_encoding, res_text, res_id, res_type, res_ver'',''res_text_vc'',%%1$L,''res_type'')',
+        "res_type_value"
+      ),
+      ', '
     )
-  select pid,
-    jsonb_object_agg(key, value) as data
-  from flat
-  where jsonb_typeof(value) <> 'object'
-  group by pid;
-SELECT string_agg(
-    format(
-      'generated_views.create_json_flat_view(''public.hfj_res_ver'',''hrv.pid, partition_date, partition_id, res_deleted_at, res_version, has_tags, res_published, res_updated, res_encoding, res_text, res_id, res_type, res_ver'',''res_text_vc'',%%1$L,''res_type'')',
-      "res_type_value"
-    ),
-    ', '
-  )
-FROM (
-    SELECT DISTINCT all_fhir_resources_all_versions.res_type AS "res_type_value"
-    FROM public."hfj_res_ver" all_fhir_resources_all_versions
-    WHERE all_fhir_resources_all_versions.res_text_vc IS NOT NULL
-    ORDER BY 1
-  ) s;
+  FROM (
+      SELECT DISTINCT all_fhir_resources_all_versions.res_type AS "res_type_value"
+      FROM public."hfj_res_ver" all_fhir_resources_all_versions
+      WHERE all_fhir_resources_all_versions.res_text_vc IS NOT NULL
+      ORDER BY 1
+    ) s;
 $ex$
 ) INTO the_queries;
 EXECUTE format(
