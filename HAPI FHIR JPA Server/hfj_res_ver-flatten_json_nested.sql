bc05c193180f5c2f2c477b43b4a71579d9b746f2
Levi <muhammad.levi@sid-indonesia.org>
2023-12-29 10:46:02 +0700

    Affected Issue(s): #114
    What this commit has achieved:
    1. Test revert to `CREATE OR REPLACE` due to
    ```
    ERROR:  cannot drop view generated_views."Observation_all_versions_view" because other objects depend on it
    ```

--- HAPI FHIR JPA Server/hfj_res_ver-flatten_json_nested.sql
+++ HAPI FHIR JPA Server/hfj_res_ver-flatten_json_nested.sql
@@ -54,8 +54,8 @@ identifier_value
 ) into cols;
 execute format (
   $ex$
-  DROP VIEW IF EXISTS generated_views."%4$s_all_versions_view";
-  CREATE VIEW generated_views."%4$s_all_versions_view" AS
+  -- DROP VIEW IF EXISTS generated_views."%4$s_all_versions_view";
+  CREATE OR REPLACE VIEW generated_views."%4$s_all_versions_view" AS
   SELECT %2$s,
     %6$s,
     CONCAT(%4$L, '/', 
